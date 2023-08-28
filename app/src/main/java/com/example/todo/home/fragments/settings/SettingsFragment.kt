e0c3e0bd80cbb4b0480c6066c5082d5d5b0d435e
ziad328 <ziad32seleem@gmail.com>
2023-08-28 17:25:33 +0300

    + settings Fragment dark mode

--- app/src/main/java/com/example/todo/home/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/todo/home/fragments/settings/SettingsFragment.kt
@@ -4,19 +4,71 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.example.todo.R
 import com.example.todo.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), AdapterView.OnItemSelectedListener {
     lateinit var viewBinding: FragmentSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         viewBinding = FragmentSettingsBinding.inflate(
             inflater, container, false
         )
         return viewBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initSpinner()
+
+    }
+
+    private fun initSpinner() {
+        initModeSpinner()
+    }
+
+    private fun initModeSpinner() {
+        viewBinding.modeSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.modes_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            viewBinding.modeSpinner.adapter = adapter
+        }
+    }
+
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var itemClicked = parent?.getItemAtPosition(position)
+        setMode(itemClicked)
+    }
+
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+    }
+
+
+    private fun setMode(itemClicked: Any?) {
+        when (itemClicked) {
+            "Light" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            "Dark" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+
 }
